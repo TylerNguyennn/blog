3d649c0e9923a7a5d2f56de4a9efbf199064be4b
TylerNguyennn <tainh.work@gmail.com>
2025-08-17 21:56:05 +0700

    update adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
   output: 'hybrid',
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel(),
   integrations: [mdx(), sitemap(), tailwind()]
 });
